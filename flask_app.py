00f82a3eb8bac9207eccabdf6733f04e00ad7f17
leejaeho <leejaeho@DESKTOP-C72BPCQ>
2022-12-08 15:33:04 +0900

    - abc test

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import render_template
 app = Flask (__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!1234'
+    
+@app.route('/abc')
+def abc():
+    return render_template("abc.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000', debug=True)
